c8edfd61dfe09d1cf2000e9809cbb9077eeb3fe7
Brandie Roberts <brandiecroberts@gmail.com>
2023-01-18 14:47:34 -0800

    Added an input file

---
+++ input.js
@@ -0,0 +1,23 @@
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function(data) {
+  // your code here
+  console.log(data);
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,23 +1,25 @@
 const connect = require("./client");
+const setupInput = require('./input');
+
 console.log("Connecting ...");
 connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
+// const setupInput = function() {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//   stdin.on("data", handleUserInput);
 
-  return stdin;
-};
+//   return stdin;
+// };
 
-const handleUserInput = function(data) {
-  // your code here
-  console.log(data);
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
+// const handleUserInput = function(data) {
+//   // your code here
+//   console.log(data);
+//   if (data === '\u0003') {
+//     process.exit();
+//   }
+// };
 
 setupInput();
