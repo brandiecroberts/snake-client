bd52f7dce37dce6891d06572b8dfd24beb789fe8
Brandie Roberts <brandiecroberts@gmail.com>
2023-01-18 16:20:44 -0800

    Modified code to allow snake to move via keys

--- client.js
+++ client.js
@@ -1,7 +1,11 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 const connect = function() {
   const conn = net.createConnection({
+    //ask mentor how to fix below
+    IP,
+    PORT,
     host: 'localhost', // IP address here,
     port: 50541, // PORT number here,host
   });
@@ -9,33 +13,20 @@ const connect = function() {
     console.log('Successfully connected to the game server');
     conn.write('Name: BCR');
   });
+  conn.on("connect", () => {
+    conn.write('Say: Hey there cowboy!');
+  });
 
-  // conn.on("connect", () => {
-  //   setTimeout(() => {
-  //     conn.write('Move: up');
-  //   });
-   
-  //   setTimeout(() => {
-  //     conn.write('Move: down');
-  //   }, 50);
-
-  //   setTimeout(() => {
-  //     conn.write('Move: left');
-  //   }, 100);
-
-  //   setTimeout(() => {
-  //     conn.write('Move: right');
-  //   }, 150);
-  // });
-
+  
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
-
+  
+  
   conn.on("data", (data) => {
     // code that does something when the connection is first established
     console.log(data);
   });
+
   
   return conn;
 };
@@ -44,3 +35,22 @@ const connect = function() {
 // connect();
 
 module.exports = connect;
+
+
+// conn.on("connect", () => {
+//   setTimeout(() => {
+//     conn.write('Move: up');
+//   });
+ 
+//   setTimeout(() => {
+//     conn.write('Move: down');
+//   }, 50);
+
+//   setTimeout(() => {
+//     conn.write('Move: left');
+//   }, 100);
+
+//   setTimeout(() => {
+//     conn.write('Move: right');
+//   }, 150);
+// });
--- input.js
+++ input.js
@@ -1,4 +1,5 @@
-
+const connect = require("./client");
+const conn = connect();
 
 const setupInput = function() {
   const stdin = process.stdin;
@@ -15,6 +16,26 @@ const handleUserInput = function(data) {
   console.log(data);
   if (data === '\u0003') {
     process.exit();
+  }
+  if (data === "w") {
+    // connect.on("data", () => {
+    conn.write("Move: up");
+    // });
+  }
+  if (data === "a") {
+    // connect.on("data", () => {
+    conn.write("Move: left");
+    // });
+  }
+  if (data === "s") {
+    // connect.on("data", () => {
+    conn.write("Move: down");
+    // });
+  }
+  if (data === "d") {
+    // connect.on("data", () => {
+    conn.write("Move: right");
+    // });
   }
 };
 
